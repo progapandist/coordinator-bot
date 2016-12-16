e041639c96df713af3f6ba88dd4b58ef2a8c5a38
Andy B <andybarnov@gmail.com>
2016-12-16 18:01:18 +0300

    bot at the stage of Part 1 ot the tutorial

--- bot.rb
+++ bot.rb
@@ -11,25 +11,10 @@ API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
-  case message.text
-  when /coord/i, /gps/i # We use regexp to react to any command that will have this combinations of symbols
-    message.reply("Give me the address!")
-    # process_coordinates # we call a separate function that will handle GPS look-up and talk to you back
-  end
-end
-
-def process_coordinates
-  Bot.on :message do |message|
-    puts "Received '#{message.inspect}' from #{message.sender}" # debug purposes
-    parsed_response = get_parsed_response(API_URL, message.text) # talk to Google API
-    unless parsed_response # The input was unintelligible
-      message.reply(text: "I did not quite get that. Bye!")
-      return # bye bye
-    end
-    message.type # trick user into thinking we type something with our fingers, HA HA HA
-    coord = extract_coordinates(parsed_response) # we have a separate method for that
-    message.reply(text: "Latitude: #{coord['lat']}, Longitude: #{coord['lng']}. Bye!")
-  end
+  parsed_response = get_parsed_response(API_URL, message.text) # talk to Google API
+  message.type # trick user into thinking we type something with our fingers, HA HA HA
+  coord = extract_coordinates(parsed_response) # we have a separate method for that
+  message.reply(text: "Latitude: #{coord['lat']}, Longitude: #{coord['lng']}")
 end
 
 def get_parsed_response(url, query)
