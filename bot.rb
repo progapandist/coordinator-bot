fff96e8c6b2a882d99a4ad08a0b728b24b718e3b
Andy B <andybarnov@gmail.com>
2016-12-19 23:00:39 +0300

    implement quick replies for greeting

--- bot.rb
+++ bot.rb
@@ -10,59 +10,48 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='.freeze
 
 IDIOMS = {
-  not_found: 'Did not quite get that. Come again, please!',
-  ask_location: 'Where do you think you are?'
+  not_found: 'There were no resutls. Ask me again, please',
+  ask_location: 'Enter destination'
 }.freeze
 
-# Don't forget to enable messaging_postbacks in console
-Bot.on :postback do |postback|
-  sender_id = postback.sender['id']
-  case postback.payload
-  when 'COORDINATES'
-    puts "processing coordinates"
-    say(sender_id, IDIOMS[:ask_location], [{content_type: 'text', title: 'Moscow', payload: 'MOSCOW'}])
-    process_coordinates
-  when 'FULL_ADDRESS'
-    show_full_address
-  end
-end
-
 # helper function to send messages declaratively
-def say(recipient_id, text, quick_replies = nil, attachment = nil)
+def say(recipient_id, text, quick_replies = nil)
   message_options = {
   recipient: { id: recipient_id },
   message: { text: text }
   }
-
   if quick_replies
     message_options[:message][:quick_replies] = quick_replies
   end
-
-  if attachment
-    message_options[:message][:attachment] = attachment
-  end
-
   Bot.deliver(message_options, access_token: ENV['ACCESS_TOKEN'])
 end
 
-def wait_for_user_input
+
+def wait_for_any_input
+  qr1 = {
+    content_type: 'text',
+    title: 'Coordinates',
+    payload: 'COORDINATES'
+  }
+
+  qr2 = {
+    content_type: 'text',
+    title: 'Full address',
+    payload: 'FULL_ADDRESS'
+  }
+
   Bot.on :message do |message|
-    puts message.sender['id']
+    sender_id = message.sender['id']
+    say(sender_id, 'What would you like to know?', [qr1, qr2])
+    wait_for_command
+  end
+end
+
+
+def wait_for_command
+  Bot.on :message do |message|
+    puts "Received '#{message.inspect}' from #{message.sender}" # debug only
     case message.text
-    # when 'hey'.downcase, 'hi'.downcase, /hello/i
-    #   say(sender_id, 'What do you want to do?', attachment: {
-    #     type: 'template',
-    #     payload: {
-    #       template_type: 'button',
-    #       text: 'Hello! How can I help you',
-    #       buttons: [
-    #         { type: 'postback', title: 'Give me my coordinates',
-    #                             payload: 'COORDINATES' },
-    #         { type: 'postback', title: 'Give me my full postal address',
-    #                             payload: 'FULL_ADDRESS' }
-    #       ]
-    #     }
-    #   })
     when /coord/i, /gps/i
       message.reply(text: IDIOMS[:ask_location])
       process_coordinates
@@ -70,26 +59,6 @@ def wait_for_user_input
       message.reply(text: IDIOMS[:ask_location])
       show_full_address
     end
-  end
-end
-
-def greet_human
-  Bot.on :message do |message|
-    message.reply(
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'Hello! How can I help you',
-          buttons: [
-            { type: 'postback', title: 'Give me my coordinates',
-                                payload: 'COORDINATES' },
-            { type: 'postback', title: 'Give me my full postal address',
-                                payload: 'FULL_ADDRESS' }
-          ]
-        }
-      }
-    )
   end
 end
 
@@ -110,16 +79,14 @@ end
 # DRY-out the bot wrapper
 def handle_api_request
   Bot.on :message do |message|
-    puts "Received '#{message.inspect}' from #{message.sender}" # debug only
     parsed_response = get_parsed_response(API_URL, message.text)
-    unless parsed_response
-      message.reply(text: IDIOMS[:not_found])
-      wait_for_user_input
-      return
-    end
     message.type # let user know we're doing something
-    yield(parsed_response, message)
-    wait_for_user_input
+    if parsed_response
+      yield(parsed_response, message)
+    else
+      message.reply(text: IDIOMS[:not_found])
+    end
+    wait_for_any_input
   end
 end
 
@@ -138,4 +105,37 @@ def extract_full_address(parsed)
 end
 
 # launch the loop
-wait_for_user_input
+wait_for_any_input
+
+# Don't forget to enable messaging_postbacks in console
+# Bot.on :postback do |postback|
+#   sender_id = postback.sender['id']
+#   case postback.payload
+#   when 'COORDINATES'
+#     puts "processing coordinates"
+#     say(sender_id, IDIOMS[:ask_location], [{content_type: 'text', title: 'Moscow', payload: 'MOSCOW'}])
+#     process_coordinates
+#   when 'FULL_ADDRESS'
+#     show_full_address
+#   end
+# end
+
+# def greet_human
+#   Bot.on :message do |message|
+#     message.reply(
+#       attachment: {
+#         type: 'template',
+#         payload: {
+#           template_type: 'button',
+#           text: 'Hello! How can I help you',
+#           buttons: [
+#             { type: 'postback', title: 'Give me my coordinates',
+#                                 payload: 'COORDINATES' },
+#             { type: 'postback', title: 'Give me my full postal address',
+#                                 payload: 'FULL_ADDRESS' }
+#           ]
+#         }
+#       }
+#     )
+#   end
+# end
