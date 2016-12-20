257be326a75507f2fbdb27cb976d7a5d3decdca0
Andy B <andybarnov@gmail.com>
2016-12-20 15:20:43 +0300

    ready to test stage 3

--- bot.rb
+++ bot.rb
@@ -9,45 +9,50 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV['ACCESS_TOKEN'])
 
 API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='.freeze
 
+IDIOMS = {
+  not_found: 'There were no resutls. Ask me again, please',
+  ask_location: 'Enter destination'
+}.freeze
+
 def wait_for_user_input
   Bot.on :message do |message|
     case message.text
     when /coord/i, /gps/i # we use regexp to match parts of strings
-      message.reply(text: 'Enter destination')
+      message.reply(text: IDIOMS[:ask_location])
       process_coordinates
     when /full ad/i # we got the user even if he misspells address
-      message.reply(text: 'Enter destination')
+      message.reply(text: IDIOMS[:ask_location])
       show_full_address
     end
   end
 end
 
+
 def process_coordinates
-  Bot.on :message do |message|
-    parsed_response = get_parsed_response(API_URL, message.text)
-    if !parsed_response
-      message.reply(text: 'There were no resutls. Ask me again, please')
-      wait_for_user_input
-      return
-    end
-    message.type # let user know we're doing something
-    coord = extract_coordinates(parsed_response)
+  handle_user_command do |api_response, message|
+    coord = extract_coordinates(api_response)
     message.reply(text: "Latitude: #{coord['lat']} / Longitude: #{coord['lng']}")
-    wait_for_user_input
   end
 end
 
 def show_full_address
+  handle_user_command do |api_response, message|
+    full_address = extract_full_address(api_response)
+    message.reply(text: full_address)
+  end
+end
+
+def handle_api_request
   Bot.on :message do |message|
+    puts "Received '#{message.inspect}' from #{message.sender}" # for development only
     parsed_response = get_parsed_response(API_URL, message.text)
-    if !parsed_response
-      message.reply(text: 'There were no resutls. Ask me again, please')
+    unless parsed_response
+      message.reply(text: IDIOMS[:not_found])
       wait_for_user_input
       return
     end
     message.type # let user know we're doing something
-    full_address = extract_full_address(parsed_response)
-    message.reply(text: full_address)
+    yield(parsed_response, message)
     wait_for_user_input
   end
 end
